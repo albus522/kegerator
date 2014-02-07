89874a419ce36422fe7ccee0c7bfb8eaa7e6de37
David Genord II <david@collectiveidea.com>
2014-02-07 00:54:32 -0500

    Change to a fast looping model with almost no delays

--- kegerator.ino
+++ kegerator.ino
@@ -13,6 +13,7 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 #define ONE_WIRE_BUS 6
 #define TEMPERATURE_PRECISION 12
+#define MAX_CONVERSION_TIME 750
 
 OneWire oneWire(ONE_WIRE_BUS);
 
@@ -29,14 +30,16 @@ volatile int setTempF;
 volatile int16_t setTempRaw;
 volatile unsigned long lastSetTempUpdate;
 unsigned long duty = 7500;
-unsigned long start;
-unsigned long now;
+unsigned long lastStateChange;
 unsigned long cummulativeOff = 0;
 unsigned long lastCummulativeOff;
+unsigned long nextTempAt;
+unsigned long lastDutyUpdate;
 int16_t tempRaw;
 int16_t prevTempRaw;
 int16_t tempChange;
 unsigned long prevTempAt;
+int curState = HIGH;
 
 int16_t tempFToRaw(int temp) {
   return (temp - 32) * 5 * 128 / 9;
@@ -48,8 +51,9 @@ void setup(void)
   analogWrite(FAN_CONTROL, FAN_MIN);
 
   digitalWrite(TEC_CONTROL, HIGH);
-  start = millis();
-  lastCummulativeOff = start;
+  lastStateChange = millis();
+  lastCummulativeOff = lastStateChange;
+  lastDutyUpdate = millis();
 
   setTempF = EEPROM.read(0);
   if(setTempF > 100) setTempF = 42;
@@ -74,6 +78,10 @@ void setup(void)
   sensors.requestTemperatures();
   prevTempRaw = sensors.getTemp(internalTherm);
   prevTempAt = millis();
+
+  sensors.setWaitForConversion(false);
+  sensors.requestTemperatures();
+  nextTempAt = millis() + MAX_CONVERSION_TIME;
 
   attachInterrupt(0, decrementSetTemp, RISING); // Pin 3
   attachInterrupt(1, incrementSetTemp, RISING); // Pin 2
@@ -105,81 +113,98 @@ void changeSetTemp(int newTemp)
 void loop(void)
 {
   unsigned long timeSinceChange;
-  sensors.requestTemperatures();
+  unsigned long nextStateChange;
+  unsigned long now;
 
-  tempRaw = sensors.getTemp(internalTherm);
-  if(tempRaw == DEVICE_DISCONNECTED_RAW) {
-    tempRaw = prevTempRaw;
-  }
-  if(tempRaw != prevTempRaw) {
-    tempChange      = tempRaw - prevTempRaw;
-    prevTempRaw     = tempRaw;
-    prevTempAt      = millis();
-    timeSinceChange = 0;
-  } else {
-    timeSinceChange = millis() - prevTempAt;
-  }
+  now = millis();
+  if(sensors.isConversionAvailable(internalTherm) || (now > nextTempAt && (now - nextTempAt) < (MAX_CONVERSION_TIME * 2))) {
+    tempRaw = sensors.getTemp(internalTherm);
+    sensors.requestTemperatures();
+    nextTempAt = millis() + MAX_CONVERSION_TIME;
 
-  // Refresh because the set temp interrupt
-  // could mess this up
-  lcd.setCursor(6,0);
-  lcd.print(" Cur ");
-  lcd.print(int(sensors.rawToFahrenheit(tempRaw) * 100) / 100.0);
-
-  // 4 * 128
-  if(tempRaw > (setTempRaw + 512)) {
-    duty = MAX_DUTY;
-  } else if(tempRaw < setTempRaw) {
-    // Temp decreasing or 60 seconds since last change
-    if(tempChange < 0 || timeSinceChange > 60000) {
-      duty -= (setTempRaw - tempRaw) / 3;
-      if(duty > MAX_DUTY || duty < 1000) duty = 1000;
+    if(tempRaw == DEVICE_DISCONNECTED_RAW) {
+      tempRaw = prevTempRaw;
     }
-  } else if(tempRaw > setTempRaw) {
-    // Temp increasing or 30 seconds since last change
-    if(tempChange > 0 || timeSinceChange > 30000) {
-      duty += (tempRaw - setTempRaw) / 3;
-      if(duty > MAX_DUTY) duty = MAX_DUTY;
+    if(tempRaw != prevTempRaw) {
+      tempChange      = tempRaw - prevTempRaw;
+      prevTempRaw     = tempRaw;
+      prevTempAt      = millis();
+      timeSinceChange = 0;
+
+      // Refresh because the set temp interrupt
+      // could mess this up
+      lcd.setCursor(6,0);
+      lcd.print(" Cur ");
+      lcd.print(int(sensors.rawToFahrenheit(tempRaw) * 100) / 100.0);
+    } else {
+      timeSinceChange = millis() - prevTempAt;
     }
-  }
 
-  lcd.setCursor(7,1);
-  lcd.print("         ");
-  lcd.setCursor(7,1);
-  lcd.print(duty / 100.0);
+    // 5 * (5 / 9 * 128)
+    if(tempRaw > (setTempRaw + 355)) {
+      duty = MAX_DUTY;
+      lastDutyUpdate = now;
+    } else if(tempRaw < setTempRaw) {
+      // Temp decreasing or 60 seconds since last change
+      if((tempChange < 0 && (now < lastDutyUpdate || (now - lastDutyUpdate) > MAX_DUTY)) || timeSinceChange > 60000) {
+        duty -= (setTempRaw - tempRaw) / 3;
+        if(duty > MAX_DUTY || duty < 1000) duty = 1000;
+        lastDutyUpdate = now;
+      }
+    } else if(tempRaw > setTempRaw) {
+      // Temp increasing or 30 seconds since last change
+      if((tempChange > 0 && (now < lastDutyUpdate || (now - lastDutyUpdate) > MAX_DUTY)) || timeSinceChange > 30000) {
+        duty += (tempRaw - setTempRaw) / 3;
+        if(duty > MAX_DUTY) duty = MAX_DUTY;
+        lastDutyUpdate = now;
+      }
+    }
 
-  if(duty > (MAX_DUTY * 8 / 10)) {
-    analogWrite(FAN_CONTROL, (duty - (MAX_DUTY * 8 / 10)) * (FAN_MAX - FAN_MIN) / (MAX_DUTY * 2 / 10) + FAN_MIN);
-  } else {
-    analogWrite(FAN_CONTROL, FAN_MIN);
+    if(lastDutyUpdate == now) {
+      lcd.setCursor(7,1);
+      lcd.print("         ");
+      lcd.setCursor(7,1);
+      lcd.print(duty / 100.0);
+    }
+
+    if(duty > (MAX_DUTY * 7 / 10)) {
+      analogWrite(FAN_CONTROL, (duty - (MAX_DUTY * 7 / 10)) * (FAN_MAX - FAN_MIN) / (MAX_DUTY * 3 / 10) + FAN_MIN);
+    } else {
+      analogWrite(FAN_CONTROL, FAN_MIN);
+    }
   }
 
   now = millis();
-  delay(constrain(duty - (now - start), 0, MAX_DUTY));
-
-  int off = constrain(MAX_DUTY - duty, 0, MAX_DUTY);
-  if(off > 0) {
-    digitalWrite(TEC_CONTROL, LOW);
-    delay(off);
-    cummulativeOff += off;
-
-    digitalWrite(TEC_CONTROL, HIGH);
+  if(curState == LOW) {
+    nextStateChange = lastStateChange + ((MAX_DUTY - duty) / 10);
+    if(now > nextStateChange && (nextStateChange > (MAX_DUTY - duty) || now < lastStateChange)) {
+      digitalWrite(TEC_CONTROL, HIGH);
+      curState = HIGH;
+      if(now > lastStateChange)
+        cummulativeOff += now - lastStateChange;
+      lastStateChange = millis();
+    }
+  } else if(duty < MAX_DUTY) {
+    nextStateChange = lastStateChange + (duty / 10);
+    if(now > nextStateChange && (nextStateChange > duty || now < lastStateChange)) {
+      digitalWrite(TEC_CONTROL, LOW);
+      curState = LOW;
+      lastStateChange = millis();
+    }
   }
 
-  start = millis();
-
   // Check Cummulative Off
-
+  now = millis();
   // Has millis rolled over or has an hour passed
-  if (start < lastCummulativeOff || (start - lastCummulativeOff) > 3600000) {
-    lastCummulativeOff = start;
+  if (now < lastCummulativeOff || (now - lastCummulativeOff) > 3600000) {
+    lastCummulativeOff = now;
     // If off less than 60 seconds
     // Give the TEC a break
     if(cummulativeOff < 60000) {
       digitalWrite(TEC_CONTROL, LOW);
       delay(constrain((60000 - cummulativeOff) * 2, 0, 60000));
       digitalWrite(TEC_CONTROL, HIGH);
-      start = millis();
+      lastStateChange = millis();
     }
     cummulativeOff = 0;
   }
